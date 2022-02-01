54d451e3f455a04a0bfdffc60b26ece9811e6a5b
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-01 14:05:33 +1100

    Adding quill to add customized notes, finish journals logic, added some magical js as well

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -45,4 +45,73 @@ const confirmation = function(){
     
 }
 
+//Creating note using Rich Editor
+const creatingEditor = function(){
+    const form = document.querySelector("#journal_form");
+    if (form !== undefined && form !== null){
+        const formTrigger = form.querySelector("input[type='submit']");
+        const notesValue  = form.querySelector("#journal_notes");
+        const divEditor = document.querySelector("#editor");
+        const editing_note = divEditor.getAttribute('editing_note')
+        const quill = new Quill(divEditor, {
+            modules: {
+              toolbar: [
+                [{'header': [1,2,3,4,5,6, false]}],
+                [{'size':['small',false,'large','huge']}],
+                ['bold', 'italic', 'underline', 'strike',{'color':[]}],
+                ['link', 'blockquote', 'code-block', 'image'],
+                [{ list: 'ordered' }, { list: 'bullet' }]
+              ]
+            },
+            theme: 'snow'  // or 'bubble' to show notes without toolbar
+          });
+        if(editing_note !== "undefined" && editing_note !== null && editing_note !== ""){
+            const parseNote = parseNotes(editing_note);
+            parseNote !== false ?  quill.setContents(parseNote) : quill.setContents([{ insert: editing_note}])
+            quill.enable(true);
+        }
+        formTrigger.addEventListener('click',function(event){
+            event.preventDefault();
+            const content = JSON.stringify(quill.getContents()); //Get content of rich editor and parse to string to be able to save in the DB
+            notesValue.value  = content;
+            form.submit();
+        });
+        
+    }
+}
+
+
+
+//Waching notes
+const editingNotes = function(){
+      const allNotes = document.querySelectorAll(".view-note-unique");
+      if (allNotes.length > 1){
+          allNotes.forEach(function(item){
+                const notes = item.children[0];
+                const quill = item.children[1];
+                const editor = new Quill(quill, {
+                    modules: {},
+                    theme: 'bubble'  // or 'bubble' to show notes without toolbar
+                  });
+                  editor.enable(false);
+                  const parsedNote = parseNotes(notes.value)
+                  parsedNote !== false ?  editor.setContents(parsedNote) : editor.setContents([{ insert: notes.value}])
+          })
+      }
+}
+
+const parseNotes = function(notes){
+    try {
+        const noteStr = JSON.parse(notes);
+        if (noteStr && typeof noteStr === "object") {
+            return noteStr
+        }
+    }
+    catch (e) {
+        return false;
+     }
+}
+
 confirmation();
+creatingEditor();
+editingNotes();
--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,21 +1,34 @@
 class JournalsController < ApplicationController
   
   def new
+    @journal = Journal.new
   end
-
   def create
   end
 
   def show
+    @journal = Journal.find(params[:id])
+    respond_to do |format|
+      format.js {render "show.js.erb"}
+      format.html { render :show } #, flash[:success] = "holder updated")
+    end
   end
 
   def edit
+    @journal = Journal.find(params[:id])
   end
 
   def update
   end
 
   def create
+    @journal = Journal.new(notes: params[:journal][:notes], trade_id: params[:journal][:trade_id], journal_date: DateTime.now.strftime('%m-%d-%Y'))
+    @journal.save
+    if  @journal.persisted?
+      redirect_to trades_path
+    else
+      render :new                                                                                                                                                
+    end
   end
 
   def index
