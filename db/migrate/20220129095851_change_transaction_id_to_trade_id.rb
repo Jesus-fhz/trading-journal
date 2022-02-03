1d5ca3b0c5a8e0962d47fc43ddef413590a9c6e1
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-03 18:50:07 +1100

    deleting migration

--- db/migrate/20220129095851_change_transaction_id_to_trade_id.rb
+++ db/migrate/20220129095851_change_transaction_id_to_trade_id.rb
@@ -1,5 +1,5 @@
 class ChangeTransactionIdToTradeId < ActiveRecord::Migration[5.2]
   def change
-    rename_column :journals, :transaction_id, :trade_id
+    #rename_column :journals, :transaction_id, :trade_id
   end
 end
